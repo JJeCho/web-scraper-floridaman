3c009a4d76fc20aed406735c96446265ee084130
JJeCho <james.je.cho@gmail.com>
2024-03-27 15:50:21 -0700

    Currently trying to adapt Multiverse HTML template for use in react app
    Animation, blur, and transitions still missing

--- web-scraper-client/src/App.js
+++ web-scraper-client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Articles from './components/Articles';
+import Multiverse from './components/Multiverse';
 
 function App() {
   return (
     <div className="App">
-      <Articles />
+      <Multiverse />
     </div>
   );
 }
